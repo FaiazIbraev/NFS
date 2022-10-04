7f9d3938da8775d99b7d8f428b60534763a56755
FaiazIbraev <fayaz.ibraev@gmail.com>
2022-10-04 13:53:14 +0600

    Initial Commit

---
+++ neo_finance_system/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  neo_finance_system
+//
+//  Created by Faiaz Ibraev on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
